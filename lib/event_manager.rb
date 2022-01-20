53f005016948f5a7761c0cc8af7faae517a0f064
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2022-01-20 14:31:06 -0600

    Update begin rescue for legislator names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,37 @@
-puts 'Event Manger Initialized!'
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatoryLowerBody']
+    )
+    legislators = legislators.officials
+
+    legistlators_names = legislators.map(&:name)
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+  puts "#{name} #{zipcode} #{legistlators_names}"
+end
+
